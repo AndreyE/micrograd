bdd262bf22c567e5a75ace1709db56daf35c44e7
Andrey Lizunov <anlizev@yandex-team.ru>
2024-05-22 12:46:30 +0300

    Add tanh non-linearity

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,3 +1,4 @@
+import math
 
 class Value:
     """ stores a single scalar value and its gradient """
@@ -47,6 +48,15 @@ class Value:
 
         def _backward():
             self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+
+        return out
+
+    def tanh(self):
+        out = Value(math.tanh(self.data), (self,), 'tanh')
+
+        def _backward():
+            self.grad += (1 - out.data**2) * out.grad
         out._backward = _backward
 
         return out
--- micrograd/nn.py
+++ micrograd/nn.py
@@ -12,20 +12,26 @@ class Module:
 
 class Neuron(Module):
 
-    def __init__(self, nin, nonlin=True):
+    def __init__(self, nin, act='linear'):
         self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
         self.b = Value(0)
-        self.nonlin = nonlin
+        self.act = act
 
     def __call__(self, x):
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
-        return act.relu() if self.nonlin else act
+        if self.act == 'relu':
+            return act.relu()
+        elif self.act == 'tanh':
+            return act.tanh()
+
+        assert self.act == 'linear'
+        return act
 
     def parameters(self):
         return self.w + [self.b]
 
     def __repr__(self):
-        return f"{'ReLU' if self.nonlin else 'Linear'}Neuron({len(self.w)})"
+        return f"{self.act}-Neuron({len(self.w)})"
 
 class Layer(Module):
 
@@ -44,9 +50,9 @@ class Layer(Module):
 
 class MLP(Module):
 
-    def __init__(self, nin, nouts):
+    def __init__(self, nin, nouts, act='tanh'):
         sz = [nin] + nouts
-        self.layers = [Layer(sz[i], sz[i+1], nonlin=i!=len(nouts)-1) for i in range(len(nouts))]
+        self.layers = [Layer(sz[i], sz[i+1], act=act if i!=len(nouts)-1 else 'linear') for i in range(len(nouts))]
 
     def __call__(self, x):
         for layer in self.layers:
