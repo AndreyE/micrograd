7d28bfb28108c88b7ebda0749b4d464e4e959047
Andrey Lizunov <anlizev@yandex-team.ru>
2024-05-24 09:50:14 +0300

    Add squeeze

--- micrograd/nn.py
+++ micrograd/nn.py
@@ -13,6 +13,11 @@ class Module:
                 return False
         return True
 
+    def init_lr(self):
+        for p in self.parameters():
+            assert p.grad != 0.0
+            p.lr = abs(1.0 / p.grad)
+
     def parameters(self):
         return []
 
@@ -30,6 +35,8 @@ class Neuron(Module):
             return act.relu()
         elif self.act == 'tanh':
             return act.tanh()
+        elif self.act == 'squeeze':
+            return act.squeeze()
 
         assert self.act == 'linear'
         return act
